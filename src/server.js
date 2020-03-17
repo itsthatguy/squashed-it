24234829c41459eeccdfb21f12dafeec7b674fb0
Kevin Altman <kevin@itsthatguy.com>
2020-03-17 16:00:47 -0400

    follow heroku guide for websockets

--- src/server.js
+++ src/server.js
@@ -1,15 +1,14 @@
 var express = require('express');
-var app = express();
+var server = express();
 var path = require('path');
 const WebSocket = require('ws');
 const http = require('http');
 
-app.use(express.static(path.join(__dirname, 'client')))
-app.get('/', function(req, res) {
+server.use(express.static(path.join(__dirname, 'client')))
+server.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'client', 'index.html'));
 });
 
-const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 const broadcast = (data) => {
@@ -21,10 +20,6 @@ const broadcast = (data) => {
 };
 
 wss.on('connection', function(ws, request) {
-  // const userId = request.session.userId;
-
-  // map.set(userId, ws);
-
   ws.on('message', function(message) {
     if (message == "playsound") {
       broadcast('playsound');
@@ -33,23 +28,8 @@ wss.on('connection', function(ws, request) {
   });
 
   ws.on('close', function() {
-    // map.delete(userId);
   });
 });
-
-// app.ws('/', function(ws, req) {
-//   ws.on('message', function(msg) {
-//     if (msg == "playsound") {
-//       ws.send('playsound');
-//       console.log(ws);
-//     }
-//   });
-
-//   ws.on('message', function(msg) {
-//     console.log(msg);
-//   });
-//   console.log('socket', req.testing);
-// });
 
 const port = process.env.PORT || 8080
 server.listen(port, () => console.log(`Server listening on http://localhost:${port}.`));
