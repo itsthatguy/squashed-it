a5e724a24fda3ccac15877b3ffb5d527cc1e4fa5
Kevin Altman <kevin@itsthatguy.com>
2020-03-17 16:19:38 -0400

    hacking the autoplay policy

--- src/client/app.js
+++ src/client/app.js
@@ -1,3 +1,6 @@
+const iframe = document.querySelector('#audio')
+const player = document.querySelector('#player')
+
 var HOST = location.origin.replace(/^http/, 'ws')
 var sock = new WebSocket(HOST);
  sock.addEventListener('open', function() {
@@ -6,8 +9,9 @@ var sock = new WebSocket(HOST);
 
  sock.addEventListener('message', function(e) {
      if (e.data == "playsound") {
-       console.log('PLAY SOUND');
-       new Audio('./dance-party.mp3').play()
+        console.log('PLAY SOUND');
+        player.volume = 1
+        player.play()
      }
  });
 
@@ -18,3 +22,8 @@ var sock = new WebSocket(HOST);
 document.querySelector('.clickme').addEventListener('click', () => {
   sock.send('playsound');
 });
+
+iframe.volume = 0
+player.volume = 0
+
+setInterval(() => sock.send('heartbeat'), 5000)
